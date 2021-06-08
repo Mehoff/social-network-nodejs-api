3c2333cf70f4767f56637ebf000c0595658c3702
Ilya Mihov <mixov2000@gmail.com>
2021-06-08 19:46:58 +0300

    getUser(id), getAllUsers()

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 if(dotenv.error){
     throw error;
@@ -17,39 +17,106 @@ const connectionOptions = {
     port: process.env.DB_PORT
 }
 
-const connection = mysql.createConnection(connectionOptions);
+async function getAllUsers(){
+    console.log('getAllUsers()')
 
-connection.connect((err) => {
-    if(err){
-        console.log(`<!> ERROR(connection.connect): \n${err}`)
-        return;
-    }
+    const connection = await mysql.createConnection(connectionOptions);
+    
+    await connection.connect((err) => {
+        if(err){
+            return {"error" : "Error while connecting to database", "innerException" : err}
+        }
+    })
 
-    console.log('Подключение успешно!');
+    const data = await connection.execute(
+        'SELECT * FROM `Users`'
+    );
 
-})
+    connection.end();
+    return data[0];
+}
 
-connection.end((err) => {
-    if(err){
-        console.log(`<!> ERROR(connection.end): \n${err}`)
-        return;
-    }
-    console.log('Подключение разъединено!')
-})
+async function getUser(id){
+    console.log(`getUser(${id})`)
+    const connection = await mysql.createConnection(connectionOptions);
+    
+    await connection.connect((err) => {
+        if(err){
+            return {"error" : "Error while connecting to database", "innerException" : err}
+        }
+    })
 
+    const data = await connection.execute(
+        `SELECT * FROM \`Users\` WHERE Id = ${id}`
+    );
 
-// EXPRESS API
+    connection.end();
+    return data[0];
+}
+
 // app.get('/', (req, res) => {
-//     res.send('Hello World');
+//     console.log(req)
+//     res.status(200).send(req.body);
 // })
 
-// app.get('/users/', (req, res) => {
-//     res.send({
-//         "firstName": "Illia",
-//         "secondName": "Mikhow",
-//         "age": "11",
+app.get('/users', async (req, res) => {
+
+    let response;
+
+    if(req.query.id){
+        response = await getUser(req.query.id);
+        
+        if(response.error){
+            res.send(response.error)
+        }
+        res.status(200).send(response)
+    }
+
+    else{
+        response = await getAllUsers();
+
+        if(response.error){
+            res.send(users.error);
+        }
+        res.status(200).send(response);
+    }
+    return res;
+
+})
+
+app.listen(process.env.PORT)
+console.log(`Listening on port ${process.env.PORT}`)
+
+
+
+
+
+
+
+// connection.connect( async (err) => {
+//     if(err){
+//         console.log(`<!> ERROR(connection.connect): \n${err}`)
+//         return;
+//     }
+    
+//     console.log('Подключение успешно!');
+
+//     await connection.query('INSERT INTO `Users` VALUES ()', (err, results, fields) => {
+//         if(err){
+//             console.log(`<!> ERROR(connection.query): \n${err}`);
+//             return;
+//         }
+//         console.log('Запрос выполнен без ошибок!')
+
+//         console.log(results);
+//         console.log(fields);
+//     }).then(() => {
+//         connection.end((err) => {
+//             if(err){
+//                 console.log(`<!> ERROR(connection.end): \n${err}`)
+//                 return;
+//             }
+//             console.log('Подключение разъединено!')
+//         })
 //     })
 // })
-
-// app.listen(process.env.PORT)
-// console.log(`Listening on port ${process.env.PORT}`)
